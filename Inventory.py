ecc4c573bb9fd5339f1596aecff0b5f27fc5004c
sebastianaldi17 <sebastian.aldi17@gmail.com>
2021-01-28 20:36:33 +0700

    Added Inventory.py

---
+++ Inventory.py
@@ -0,0 +1,17 @@
+class Inventory:
+    def __init__(self):
+        self.items = {}
+        # Key is item.name
+        # Value is [item, quantity]
+
+    def add_item(self, item):
+        if item.name not in self.item:
+            self.items[item.name] = [item, 1]
+        else:
+            self.items[item.name][1] += 1
+
+    def remove_item(self, item):
+        if item.name not in self.item:
+            self.items[item.name] = 1
+        else:
+            self.items[item.name] -= 1
--- monster.py
+++ monster.py
@@ -1,10 +1,13 @@
 class Monster:
-    def __init__(self, name, hp, max_hp, attack, defence, loot):
+    def __init__(self, name, hp, max_hp, attack, defence, skills, loot):
         self.name = name
         self.max_hp = max_hp
         self.hp = hp
         self.attack = attack
         self.defence = defence
+
+        # Skills should be a list of skills [skill1, skill2]
+        self.skills = skills
 
         # Loot can be either a list or a dict (for probability drops)
         self.loot = loot
